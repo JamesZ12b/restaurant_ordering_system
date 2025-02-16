c2340d6bfa8d1f10f5e7e253cdfff8fcac66e5e2
Jamesz12b <kaixin.zhan@my.jcu.edu.au>
2025-02-17 00:54:19 +0800

    Make the Table list,easily to test QRcode

--- qrcode_app/urls.py
+++ qrcode_app/urls.py
@@ -1,9 +1,10 @@
 # qrcode_app/urls.py
 
 from django.urls import path
-from .views import create_table, view_qr_code
+from .views import create_table, view_qr_code, table_list
 
 urlpatterns = [
-    path('create/', create_table, name='create_table'),
-    path('qr_code/<str:table_number>/', view_qr_code, name='view_qr_code'),  # 确保这个路由存在
+    path('', table_list, name='table_list'),  # 餐桌列表
+    path('create/', create_table, name='create_table'),  # 创建餐桌
+    path('qr_code/<str:table_number>/', view_qr_code, name='qr_code_view'),  # 餐桌二维码视图
 ]
