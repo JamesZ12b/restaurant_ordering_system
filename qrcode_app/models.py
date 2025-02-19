f615952edc8116814e97447ecb8270b5b8b3e38d
Jamesz12b <kaixin.zhan@my.jcu.edu.au>
2025-02-20 00:03:41 +0800

    Add pictures on docker and make a javascript of option on add cart,improve some html design, tough work

--- qrcode_app/models.py
+++ qrcode_app/models.py
@@ -10,10 +10,18 @@ class Table(models.Model):
         return self.table_number
 
 class MenuItem(models.Model):
+    CATEGORY_CHOICES = [
+        ('Main Course', 'Main Course'),
+        ('Snack', 'Snack'),
+        ('Drink', 'Drink'),
+        ('Dessert', 'Dessert'),
+    ]
+
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    photo = models.ImageField(upload_to='menu_photos/')
+    image = models.ImageField(upload_to='menu_items/', blank=True, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='Main Course')  #
 
     def __str__(self):
         return self.name
--- qrcode_app/urls.py
+++ qrcode_app/urls.py
@@ -1,5 +1,7 @@
 # qrcode_app/urls.py
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from .views import create_table, view_qr_code, table_list, menu_view
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('qr_code/<str:table_number>/', view_qr_code, name='qr_code_view'),
     path('menu/<str:table_number>/', menu_view, name='menu_view')# 餐桌二维码视图
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
