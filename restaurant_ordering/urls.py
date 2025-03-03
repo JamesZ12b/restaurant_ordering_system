bbf6c45c5d0c41bd9244a779fb311a54e4a2a122
Jamesz12b <kaixin.zhan@my.jcu.edu.au>
2025-03-03 12:57:10 +0800

    Add the stipe payment with it

--- restaurant_ordering/urls.py
+++ restaurant_ordering/urls.py
@@ -14,17 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# restaurant_ordering_system/urls.py
-
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('table/', include('qrcode_app.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
